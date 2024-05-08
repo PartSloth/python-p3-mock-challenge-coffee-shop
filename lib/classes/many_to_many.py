7deb1621856733229f8eb4e1c98ede04310bc036
Louiza Mak <louiza.mak@gmail.com>
2024-05-08 15:26:17 -0500

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,75 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if hasattr(self, 'name'):
+            raise Exception
+        if isinstance(name, str) and len(name) >= 3:
+            self._name = name
+        else: 
+            raise Exception
+
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list(set([order.customer for order in self.orders()]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        return sum([order.price for order in self.orders()]) / len(self.orders())
 
 class Customer:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list(set([order.coffee for order in self.orders()]))
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
     
 class Order:
+
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price 
+
+    @price.setter
+    def price(self, price):
+        if hasattr(self, 'price'):
+            raise Exception
+        if isinstance(price, float) and 1.0 <= price <= 10.0:
+            self._price = price
+        else:
+            raise Exception
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,14 @@ from classes.many_to_many import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
+    customer = Customer("Sleepy")
+    customer_2 = Customer("Toad")
+    coffee = Coffee("Latte")
+    coffee_2 = Coffee("Frappe")
+    price = 3.0
+
+    Order(customer, coffee, price)
+    Order(customer, coffee_2, price)
+    Order(customer_2, coffee, price)
+
     ipdb.set_trace()
